f2ce75b7d513a01979158cc8876f1ba67cffa844
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-12-19 12:30:41 +0000

    Update HH_Geppetto_Script.js

--- LEMS/SingleComponentHH/HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/HH_Geppetto_Script.js
@@ -8,7 +8,9 @@ var options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:640,show:false}};
 
 Plot1.setOptions(options)
 
-Plot1.plotData(v);
+Simulation.start();
+
+setTimeout(function(){Plot1.plotData(v);}, 500);
 
 Plot1.plotData(spiking);
 
