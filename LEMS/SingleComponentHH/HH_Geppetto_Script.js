b6dc0fa60f470f6d1693176d4b51cecf9e6cbcdf
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-12-19 12:38:37 +0000

    Update HH_Geppetto_Script.js

--- LEMS/SingleComponentHH/HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/HH_Geppetto_Script.js
@@ -8,6 +8,8 @@ var options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:640,show:false}};
 
 Plot1.setOptions(options)
 
+Plot1.setName("Hodgkin-Huxley Spiking Neuron");
+
 Simulation.start();
 
 setTimeout(function(){Plot1.plotData(v);}, 500);
