594c83b21c4dc92ecdeee99d22f609f4e9fe6f00
jrmartin <jesus@metacell.us>
2014-01-30 11:25:25 -0800

    Adding SPH simulation with script

---
+++ SPH/LiquidSmall/TEST_LIQUID_SMALL_SCRIPT.js
@@ -0,0 +1,19 @@
+Simulation.addWatchLists([{name:"sphvars",variablePaths:["sph.particle[100].position.y", "sph.particle[100].position.x"]}]);
+
+Simulation.startWatch();
+
+G.addWidget(Widgets.PLOT);
+
+var options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:130,show:false}};
+
+Plot1.setOptions(options)
+
+Plot1.setPosition(194,140);
+
+Simulation.start();
+
+Plot1.plotData(hhpop[0].v);
+
+Plot1.plotData(hhpop[0].spiking);
+
+
