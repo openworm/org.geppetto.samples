e8499f8d933926f340d73843d198b56f0bb1fa7a
jrmartin <jesus@metacell.us>
2014-02-28 15:23:50 -0800

    Testing different variables

--- LEMS/SingleComponentHH/TEST_HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/TEST_HH_Geppetto_Script.js
@@ -1,10 +1,10 @@
-Simulation.addWatchLists([{name:"hhvars",variablePaths:["example1.hhpop[0].v", "example1.hhpop[0].spiking"]}]);
+Simulation.addWatchLists([{name:"hhvars",variablePaths:["example1.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q", "example1.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q"]}]);
 
 Simulation.startWatch();
 
 G.addWidget(Widgets.PLOT);
 
-var options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:140,show:false}};
+var options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:130,show:false}};
 
 Plot1.setOptions(options)
 
@@ -14,8 +14,8 @@ Plot1.setPosition(194,140);
 
 Simulation.start();
 
-Plot1.plotData(hhpop[0].v);
+Plot1.plotData(hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q);
 
-Plot1.plotData(hhpop[0].spiking);
+Plot1.plotData(hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q);
 
 
