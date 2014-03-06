6f7ac4ce7364be3676f7dd0080446387783e668e
jrmartin <jesus@metacell.us>
2014-03-05 19:40:50 -0800

    More commands to test script

--- LEMS/SingleComponentHH/TEST_HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/TEST_HH_Geppetto_Script.js
@@ -10,7 +10,7 @@ Plot1.setOptions(options)
 
 Plot1.setName("Hodgkin-Huxley Spiking Neuron");
 
-Plot1.setPosition(194,140);
+Plot1.setPosition(114,169);
 
 Simulation.start();
 
@@ -28,6 +28,22 @@ Simulation.addWatchLists([{name:"hhvars",variablePaths:["example1.hhpop[0].bioPh
 
 Plot2.setOptions(options);
 
-Plot2.setPosition(595, 300);
+Plot2.setPosition(873, 102);
 
 Plot2.plotData(hhpop[0].bioPhys1.membraneProperties.naChans.gDensity);
+
+G.wait(1500);
+
+Simulation.load("https://raw.github.com/openworm/org.geppetto.samples/master/SPH/LiquidSmall/GEPPETTO_SCRIPTED.xml")
+
+G.wait(1000);
+
+G.addWidget(Widgets.PLOT);
+
+Simulation.addWatchLists([{name:"sphvars",variablePaths:["sph.particle[1].position.z"]}]);
+
+Plot2.setOptions(options);
+
+Plot2.setPosition(773, 102);
+
+Plot2.plotData(sph.particle[1].position.z);
