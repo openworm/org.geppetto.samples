3ea60e65f25e6dcc87f9e0944447c0e77bceaa79
jrmartin <jesus@metacell.us>
2014-03-06 18:27:13 -0800

    add commands

--- LEMS/SingleComponentHH/TEST_HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/TEST_HH_Geppetto_Script.js
@@ -24,13 +24,13 @@ G.wait(1000);
 
 G.addWidget(Widgets.PLOT);
 
+Plot2.setPosition(873, 102);
+
 Simulation.addWatchLists([{name:"hhvars",variablePaths:["example1.hhpop[0].bioPhys1.membraneProperties.naChans.gDensity"]}]);
 
 options = {yaxis:{min:0,max:250},xaxis:{min:0,max:400,show:false}};
 
 Plot2.setOptions(options);
-
-Plot2.setPosition(873, 102);
 
 Plot2.plotData(hhpop[0].bioPhys1.membraneProperties.naChans.gDensity);
 
@@ -46,12 +46,28 @@ G.addWidget(Widgets.PLOT);
 
 Simulation.addWatchLists([{name:"hhvars",variablePaths:["example1.hhpop[0].debugVal", "example1.hhpop[0].spiking"]}]);
 
-options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:340,show:false}};
+options = {yaxis:{min:-.1,max:1},xaxis:{min:0,max:340,show:false}};
 
 Plot3.setOptions(options);
 
-Plot3.setPosition(460, 404);
+Plot3.setPosition(679, 401);
 
 Plot3.plotData(hhpop[0].spiking);
 
 Plot3.plotData(hhpop[0].debugVal);
+
+G.wait(1000);
+
+Plot2.removeDataSet(hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q);
+
+Plot2.removeDataSet(hhpop[0].bioPhys1.membraneProperties.naChans.gDensity);
+
+Simulation.addWatchLists([{name:"hhvars",variablePaths:["example1.hhpop[0].v", "example1.hhpop[0].spiking"]}]);
+
+options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:160,show:false}};
+
+Plot2.setOptions(options);
+
+Plot2.plotData(hhpop[0].v);
+
+Plot2.plotData(hhpop[0].spiking);
