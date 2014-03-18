b13c68e61543902dac44ce2434f12aadda4eccd3
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2014-03-18 18:56:00 +0000

    Update TEST_HH_Geppetto_Script.js

--- LEMS/SingleComponentHH/TEST_HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/TEST_HH_Geppetto_Script.js
@@ -11,7 +11,9 @@ Plot1.setOptions(options)
 
 Plot1.setName("Gating variables");
 
-Plot1.setPosition(114,169);
+Plot1.setPosition(710,90);
+
+Plot1.setSize(285,352)
 
 Simulation.start();
 
@@ -21,13 +23,15 @@ Plot1.plotState("hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q");
 
 Plot1.plotState("hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q");
 
-G.showConsole(true);
+//G.showConsole(true);
 
-G.wait(1000);
+//G.wait(1000);
 
 G.addWidget(Widgets.PLOT);
 
-Plot2.setPosition(873, 102);
+Plot2.setPosition(113, 336);
+
+Plot2.setSize(200,352)
 
 Plot1.setName("Gating variables");
 
@@ -41,29 +45,11 @@ Plot2.setOptions(options);
 
 Plot2.plotState("hhpop[0].bioPhys1.membraneProperties.naChans.gDensity");
 
-G.wait(1000);
+//G.wait(1000);
 
 G.addWidget(Widgets.PLOT);
 
-Simulation.addWatchLists([{name:"hhvars",variablePaths:["example1.hhpop[0].debugVal", "example1.hhpop[0].spiking"]}]);
-
-options = {yaxis:{min:-.1,max:1},xaxis:{min:0,max:340,show:false}};
-
-Plot3.setOptions(options);
-
-Plot3.setPosition(679, 401);
-
-Plot3.plotState("hhpop[0].spiking");
-
-Plot3.plotState("hhpop[0].debugVal");
-
-Plot3.setName("Hodgkin-Huxley Spiking debug");
-
-G.wait(1000);
-
-G.addWidget(Widgets.PLOT);
-
-Plot4.setName("Hodgkin-Huxley Spiking Neuron");
+Plot3.setName("Hodgkin-Huxley Spiking Neuron");
 
 Simulation.addWatchLists([{name:"hhvars",variablePaths:["example1.hhpop[0].v", "example1.hhpop[0].spiking"]}]);
 
@@ -71,6 +57,10 @@ options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:160,show:false}};
 
 Plot4.setOptions(options);
 
-Plot4.plotState("hhpop[0].v");
+Plot3.plotState("hhpop[0].v");
 
-Plot4.plotState("hhpop[0].spiking");
+Plot3.plotState("hhpop[0].spiking");
+
+Plot3.setPosition(113, 90);
+
+Plot3.setSize(230,352)
