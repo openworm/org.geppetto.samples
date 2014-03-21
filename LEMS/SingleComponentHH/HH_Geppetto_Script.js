0f1eaa947bbe1c34104b41b4d41501878b58af2d
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2014-03-21 13:21:07 +0000

    Add automatic lighting up

--- LEMS/SingleComponentHH/HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/HH_Geppetto_Script.js
@@ -46,6 +46,7 @@ Plot3.plotState("hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q");
 Plot3.plotState("hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q");
 Plot3.plotState("hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q");
 
+Simulation.addBrightnessFunction("hhcell", "hhpop[0].v", function(x){return (x+0.06)/0.06;});
 
 
 
