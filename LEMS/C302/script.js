9d05878062a7e80f048f400eed09521f2845bb9c
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2014-05-11 02:35:55 +0100

    caps

--- LEMS/C302/script.js
+++ LEMS/C302/script.js
@@ -1,4 +1,4 @@
-Simulation.addWatchLists([{name:"hhvars",variablePaths:["c302.electrical.ADAL.0.generic_iaf_cell.v","c302.electrical.ADAL.0.generic_iaf_cell.v","c302.electrical.bdur.0.generic_iaf_cell.v","c302.electrical.I1R.0.generic_iaf_cell.v","c302.electrical.I2L.0.generic_iaf_cell.v","c302.electrical.PVDR.0.generic_iaf_cell.v"]}]);
+Simulation.addWatchLists([{name:"hhvars",variablePaths:["c302.electrical.ADAL.0.generic_iaf_cell.v","c302.electrical.ADAL.0.generic_iaf_cell.v","c302.electrical.BDUR.0.generic_iaf_cell.v","c302.electrical.I1R.0.generic_iaf_cell.v","c302.electrical.I2L.0.generic_iaf_cell.v","c302.electrical.PVDR.0.generic_iaf_cell.v"]}]);
 Simulation.startWatch();
 Simulation.start();
 
@@ -8,14 +8,14 @@ Plot1.setOptions({yaxis:{min:-.08,max:-.04},xaxis:{min:0,max:800,show:false}});
 Plot1.setPosition(146,72);
 Plot1.plotData("c302.electrical.ADAL.0.generic_iaf_cell.v");
 Plot1.plotData("c302.electrical.ADAR.0.generic_iaf_cell.v");
-Plot1.plotData("c302.electrical.bdur.0.generic_iaf_cell.v");
+Plot1.plotData("c302.electrical.BDUR.0.generic_iaf_cell.v");
 Plot1.plotData("c302.electrical.I1R.0.generic_iaf_cell.v");
 Plot1.plotData("c302.electrical.I2L.0.generic_iaf_cell.v");
 Plot1.plotData("c302.electrical.PVDR.0.generic_iaf_cell.v");
 
 window.setTimeout(function(){Simulation.addBrightnessFunction("c302.electrical.ADAL", "c302.electrical.ADAL.0.generic_iaf_cell.v", function(x){return (x+0.06)/0.06;})},500);
 window.setTimeout(function(){Simulation.addBrightnessFunction("c302.electrical.ADAR", "c302.electrical.ADAR.0.generic_iaf_cell.v", function(x){return (x+0.06)/0.06;})},500);
-window.setTimeout(function(){Simulation.addBrightnessFunction("c302.electrical.bdur", "c302.electrical.bdur.0.generic_iaf_cell.v", function(x){return (x+0.06)/0.06;})},500);
+window.setTimeout(function(){Simulation.addBrightnessFunction("c302.electrical.BDUR", "c302.electrical.BDUR.0.generic_iaf_cell.v", function(x){return (x+0.06)/0.06;})},500);
 window.setTimeout(function(){Simulation.addBrightnessFunction("c302.electrical.I1R", "c302.electrical.I1R.0.generic_iaf_cell.v", function(x){return (x+0.06)/0.06;})},500);
 window.setTimeout(function(){Simulation.addBrightnessFunction("c302.electrical.I2L", "c302.electrical.I2L.0.generic_iaf_cell.v", function(x){return (x+0.06)/0.06;})},500);
 window.setTimeout(function(){Simulation.addBrightnessFunction("c302.electrical.PVDR", "c302.electrical.PVDR.0.generic_iaf_cell.v", function(x){return (x+0.06)/0.06;})},500);
