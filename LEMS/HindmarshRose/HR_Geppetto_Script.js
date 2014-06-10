13c39e3536946626069721e8435d5093b2cea46b
Adrian Quintana <adrian.perez@ucl.ac.uk>
2014-06-10 13:25:46 +0100

    Adding sampling option

--- LEMS/HindmarshRose/HR_Geppetto_Script.js
+++ LEMS/HindmarshRose/HR_Geppetto_Script.js
@@ -7,7 +7,8 @@ G.addWidget(2);
 Scatter3d1.setName("Hindmarsh-Rose Spiking Neuron");
 Scatter3d1.setPosition(113, 90);
 Scatter3d1.setSize(400,545); 
-//Scatter3d1.plotData(["hhcell.electrical.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q", "hhcell.electrical.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q","hhcell.electrical.hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q"]);
-Scatter3d1.plotData(["hhcell.electrical.hr_chaotic_pop[0].x", "hhcell.electrical.hr_chaotic_pop[0].y", "hhcell.electrical.hr_chaotic_pop[0].z"]);
+options = {plotEachN:10};
+Scatter3d1.plotData(["hhcell.electrical.hr_chaotic_pop[0].x", "hhcell.electrical.hr_chaotic_pop[0].y", "hhcell.electrical.hr_chaotic_pop[0].z"], options);
+
 
 
