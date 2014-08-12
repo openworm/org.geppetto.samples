02baae6cb69ab955411a78f2ca9f08e0cfd12b52
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2014-08-12 12:46:09 +0200

    Update script.js

--- Obj/Eyewire/script.js
+++ Obj/Eyewire/script.js
@@ -1,5 +1,7 @@
 G.addWidget(Widgets.POPUP);
-Popup1.setSize(196.80000019073486,447.80000019073486);
 Popup1.setName("EyeWire Sample");
 Popup1.setMessage("EyeWire (https://eyewire.org) is a game to map the brain from Sebastian Seung's Lab at MIT. This citizen science human-based computation game challenges players to map 3D neurons in a retina. Geppetto is visualizing in 3D a morphology of a Ganglion cell that was reconstructed using the game.");
-Popup1.setPosition(846,87);
+Popup1.setSize(172.80000019073486,394.80000019073486)
+Popup1.setPosition(129,77)
+G.incrementCameraZoom(-0.16)
+G.incrementCameraRotate(0, 0, 0.07)
