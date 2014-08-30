0674dc5995ffb1292cadcf5ec1e400dbf5693abf
jrmartin <jesus@metacell.us>
2014-08-30 09:12:48 -0700

    changing tests to use variable objects instead of strings

--- LEMS/SingleComponentHH/HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/HH_Geppetto_Script.js
@@ -14,8 +14,8 @@ options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:400,show:false}};
 Plot1.setOptions(options);
 Plot1.setPosition(113, 90);
 Plot1.setSize(230,445)
-Plot1.plotData("hhcell.electrical.SimulationTree.hhpop[0].v");
-Plot1.plotData("hhcell.electrical.SimulationTree.hhpop[0].spiking");
+Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].v);
+Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].spiking);
 
 
 G.wait(100);
@@ -28,7 +28,7 @@ Plot2.setPosition(113, 336);
 Plot2.setSize(245,445)
 Plot2.setName("Conductance Density");
 Plot2.setOptions(options);
-Plot2.plotData("hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.gDensity");
+Plot2.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.gDensity);
 
 //Adding Plot 3
 
@@ -42,9 +42,11 @@ Plot3.setOptions(options)
 Plot3.setName("Gating Variables");
 Plot3.setPosition(710,90);
 Plot3.setSize(285,465)
-Plot3.plotData("hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q");
-Plot3.plotData("hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q");
-Plot3.plotData("hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q");
+Plot3.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q);
+Plot3.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q);
+Plot3.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q);
+
+Plot3.setLabels("The H.Q Variable","","The H.Q Variable");
 
 G.addWidget(Widgets.POPUP);
 Popup1.setMessage("The Hodgkin-Huxley model (or conductance-based model) is a mathematical model that describes how action potentials in neurons are initiated and propagated. It is a set of nonlinear differential equations that approximates the electrical characteristics of excitable cells such as neurons.");
