3b6efca230f7823d42e0e7079f6d8a0e8c62060f
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2014-11-11 14:46:17 +0000

    Add script

---
+++ NeuroML/Pharyngeal/script.js
@@ -0,0 +1,5 @@
+pyramidal.electrical.getModelTree();
+G.addWidget(3);
+TreeVisualiserDAT1.setData(pyramidal, {expandNodes: true});
+TreeVisualiserDAT1.setPosition(140,70);
+TreeVisualiserDAT1.setSize(600,700);
