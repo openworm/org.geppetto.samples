6a72ee26d9f764931ec9eae3377e028baa148f3c
adrianq <adrianq@users.noreply.github.com>
2014-11-13 00:14:45 +0000

    Update ACnet2_Script.js

--- NeuroML/ACnet2/ACnet2_Script.js
+++ NeuroML/ACnet2/ACnet2_Script.js
@@ -1,47 +1,12 @@
-acnet2.electrical.getModelTree();
+var data = acnet2.electrical;
 
-var data = acnet2.electrical.ModelTree.Summary;
+data.getModelTree()
 
-searchNodeByMetaType(data, "FunctionNode", plotFunctionNode)
+var nodes = Simulation.searchNodeByMetaType(data.ModelTree.Summary, "FunctionNode", G.plotFunctionNode)
+
+//TODO: This needs to be changed once "addWidget" returns the widget just created
+for (var i = 0; i<nodes.length; i++){G.addWidget(Widgets.PLOT); var plotObject = eval("Plot" + (i + 1)); plotObject.plotFunctionNode(nodes[i]); plotObject.setSize(200,450);}
 
 //Adding Scatter3d 1
-//G.addWidget(3);
-//TreeVisualiserDAT1.setData(acnet2);
-
-function plotFunctionNode(data){
-	if (data.plotMetadata != null){
-		var expression = functionNode.getExpression();
-
-		var finalValue = parseFloat(functionNode.plotMetadata["FinalValue"]);
-		var initialValue = parseFloat(functionNode.plotMetadata["InitialValue"]);
-		var stepValue = parseFloat(functionNode.plotMetadata["StepValue"]);
-
-		var plotTitle = functionNode.plotMetadata["PlotTitle"];
-		var XAxisLabel = functionNode.plotMetadata["XAxisLabel"];
-		var YAxisLabel = functionNode.plotMetadata["YAxisLabel"];
-
-		var dataArray = [];
-		for (i=initialValue; i<finalValue; i = i + stepValue){dataArray.push([i]);}
-
-		options = {xaxis:{min:initialValue,max:finalValue,show:true}};
-		G.addWidget(Widgets.PLOT);
-
-		Plot1.plotDataFunction("f(v) =" + expression, dataArray, options);
-		Plot1.setName(plotTitle);
-	}
-}
-function searchNodeByMetaType(data, metaType, action){
-	if (data._metaType == metaType){
-		action(data);
-	}
-	else{
-		if (data.getChildren() != null){
-			var children = data.getChildren().models;
-			if (children.length > 0){
-				for (var childIndex in children){
-					searchNodeByMetaType(children[childIndex], "FunctionNode");
-				}
-			}
-		}
-	}
-}
+G.addWidget(3);
+TreeVisualiserDAT1.setData(acnet2);
