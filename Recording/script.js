b1ef4ac7e86bec24663c8bbd2f5f0310ed74e099
jrmartin <yeshuarmartinez@gmail.com>
2015-01-01 15:20:15 -0800

    recording script

---
+++ Recording/script.js
@@ -0,0 +1,9 @@
+Simulation.addWatchLists([{name:"hhvars",variablePaths:["Entity.Aspect.SimulationTree.P.neuron0.gi","Entity.Aspect.SimulationTree.P.neuron0.ge"]}]);
+G.addWidget(Widgets.POPUP);
+Popup1.setName("Recording Simulation");
+Popup1.setMessage("Testing recording values");
+Popup1.setSize(172.80000019073486,394.80000019073486)
+Popup1.setPosition(129,77)
+G.addWidget(Widgets.VARIABLEVISUALISER);
+VariableVisualizer1.setVariable(Entity.Aspect.SimulationTree.P.neuron0.gi);
+VariableVisualizer1.setPosition(529,77);
