6e5f6c3d02dc278f6c61139820bdad3053983f84
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-01-02 13:03:20 +0000

    Update script

--- NeuroML/ACnet2/script.js
+++ NeuroML/ACnet2/script.js
@@ -4,4 +4,11 @@ Connectivity1.setData(acnet2);
 G.addWidget(1);
 Popup1.setMessage("Network model of primary auditory cortex in rodents. David Beeman, University of Colorado, Boulder. For more information see http://opensourcebrain.org/projects/acnet2.");
 Popup1.setName("Description");
-Popup1.setPosition(854,71);
+Connectivity1.setPosition(780,182)
+G.incrementCameraPan(-0.1, 0)
+G.incrementCameraZoom(-0.1)
+G.incrementCameraPan(0, -0.1)
+Connectivity1.setPosition(645,210)
+Popup1.setPosition(641,78)
+Popup1.setSize(117.80000019073486,445.80000019073486)
+acnet2.baskets_12_0.electrical.select()
