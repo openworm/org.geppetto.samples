9141a14dfdd390cad376917093252469bfeb8e85
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-01-02 12:55:15 +0000

    Adding acnet2 sample

---
+++ NeuroML/ACnet2/script.js
@@ -0,0 +1,7 @@
+G.addWidget(6);
+Connectivity1.setName("Connectivity matrix");
+Connectivity1.setData(acnet2);
+G.addWidget(1);
+Popup1.setMessage("Network model of primary auditory cortex in rodents. David Beeman, University of Colorado, Boulder. For more information see http://opensourcebrain.org/projects/acnet2.");
+Popup1.setName("Description");
+Popup1.setPosition(854,71);
