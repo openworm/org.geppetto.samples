f7e0606a6cbf002c36996c9e7ef86fa9143e7ad0
jrmartin <yeshuarmartinez@gmail.com>
2015-01-13 09:37:22 -0800

    fix widget object reference in script

--- Recording/script.js
+++ Recording/script.js
@@ -8,5 +8,5 @@ Popup1.setMessage("Testing recording values");
 Popup1.setSize(172.80000019073486,394.80000019073486)
 Popup1.setPosition(129,77)
 G.addWidget(Widgets.VARIABLEVISUALISER);
-VariableVisualizer1.setVariable(Entity.Aspect.SimulationTree.P.neuron0.gi);
-VariableVisualizer1.setPosition(529,77);
+VarVis1.setVariable(Entity.Aspect.SimulationTree.P.neuron0.gi);
+VarVis1.setPosition(529,77);
