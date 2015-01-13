4658d7bff4513d49fc6f801781a4b7781528d698
jrmartin <yeshuarmartinez@gmail.com>
2015-01-13 10:05:05 -0800

    update reference to recordings variables

--- Recording/script.js
+++ Recording/script.js
@@ -8,5 +8,5 @@ Popup1.setMessage("Testing recording values");
 Popup1.setSize(172.80000019073486,394.80000019073486)
 Popup1.setPosition(129,77)
 G.addWidget(Widgets.VARIABLEVISUALISER);
-VarVis1.setVariable(Entity.Aspect.SimulationTree.P.neuron0.gi);
+VarVis1.setVariable(purkinje.electrical.SimulationTree.P.neuron0.gi);
 VarVis1.setPosition(529,77);
