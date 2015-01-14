ac6bcf5419eabf2086b43f75879ed985d584de58
jrmartin <yeshuarmartinez@gmail.com>
2015-01-14 06:09:17 -0800

    plotting recording values

--- Recording/script.js
+++ Recording/script.js
@@ -6,7 +6,15 @@ G.addWidget(Widgets.POPUP);
 Popup1.setName("Recording Simulation");
 Popup1.setMessage("Testing recording values");
 Popup1.setSize(172.80000019073486,394.80000019073486)
-Popup1.setPosition(129,77)
-G.addWidget(Widgets.VARIABLEVISUALISER);
-VarVis1.setVariable(purkinje.electrical.SimulationTree.P.neuron0.gi);
-VarVis1.setPosition(529,77);
+Popup1.setPosition(129,77);
+
+G.addWidget(Widgets.PLOT);
+
+Plot1.setName("Recording Simulation");
+
+options = {yaxis:{min:-.1,max:0.1},xaxis:{min:0,max:400,show:false}};
+
+Plot1.setOptions(options);
+Plot1.setPosition(113, 90);
+Plot1.setSize(230,445)
+Plot1.plotData(purkinje.electrical.SimulationTree.P.neuron0.gi);
