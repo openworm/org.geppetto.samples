c01a787e1f8e1cf9a9cb350af94a657bddd72537
jrmartin <yeshuarmartinez@gmail.com>
2015-03-27 09:51:24 -0700

    update test

--- LEMS/SingleComponentHH/HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/HH_Geppetto_Script.js
@@ -1,1 +1,3 @@
+Simulation.addWatchLists([{name:"hhvars",variablePaths:["hhcell.electrical.SimulationTree.a", "hhcell.electrical.SimulationTree.b","hhcell.electrical.SimulationTree.c","hhcell.electrical.SimulationTree.d"]}]);
+Simulation.startWatch();
 Simulation.start();
