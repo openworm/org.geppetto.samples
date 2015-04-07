001505d4f69ee802fec17592019ac43dedb0a397
jrmartin <yeshuarmartinez@gmail.com>
2015-04-07 13:03:06 -0700

    create script for launching plot widget with time series after simulation stopped

--- LEMS/SingleComponentHH/HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/HH_Geppetto_Script.js
@@ -1,3 +1,14 @@
 Simulation.addWatchLists([{name:"hhvars",variablePaths:["hhcell.electrical.SimulationTree.a","hhcell.electrical.SimulationTree.b","hhcell.electrical.SimulationTree.c", "hhcell.electrical.SimulationTree.d"]}]);
 Simulation.startWatch();
 Simulation.start();
+
+GEPPETTO.on(Events.Simulation_stopped, function(){
+	G.addWidget(GEPPETTO.Widgets.PLOT);
+
+	Plot1.setPosition(146,72);
+	Plot1.setName("Neuron Simulator Data");
+	Plot1.plotData(hhcell.electrical.SimulationTree.a);
+	Plot1.plotData(hhcell.electrical.SimulationTree.a);
+	Plot1.plotData(hhcell.electrical.SimulationTree.a);
+	Plot1.plotData(hhcell.electrical.SimulationTree.a);
+});
