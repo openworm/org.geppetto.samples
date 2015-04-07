4a13146595d5494e0d8dab075f4fe33801150682
jrmartin <yeshuarmartinez@gmail.com>
2015-04-07 14:36:48 -0700

    change

--- LEMS/SingleComponentHH/HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/HH_Geppetto_Script.js
@@ -1,5 +1,5 @@
 Simulation.addWatchLists([{name:"hhvars",variablePaths:["hhcell.electrical.SimulationTree.a","hhcell.electrical.SimulationTree.b","hhcell.electrical.SimulationTree.c", "hhcell.electrical.SimulationTree.d"]}]);
 Simulation.startWatch();
 Simulation.start();
-
-GEPPETTO.on(Events.Simulation_stopped, function(){var done=false;if(!done){G.addWidget(GEPPETTO.Widgets.PLOT);Plot1.setPosition(146,72);Plot1.setName("Neuron Simulator Data");Plot1.plotData(hhcell.electrical.SimulationTree.a);Plot1.plotData(hhcell.electrical.SimulationTree.b);Plot1.plotData(hhcell.electrical.SimulationTree.c);Plot1.plotData(hhcell.electrical.SimulationTree.d);done=true;}});
+var go = true;
+GEPPETTO.on(Events.Simulation_stopped, function(){if(go){G.addWidget(GEPPETTO.Widgets.PLOT);Plot1.setPosition(146,72);Plot1.setName("Neuron Simulator Data");Plot1.plotData(hhcell.electrical.SimulationTree.a);Plot1.plotData(hhcell.electrical.SimulationTree.b);Plot1.plotData(hhcell.electrical.SimulationTree.c);Plot1.plotData(hhcell.electrical.SimulationTree.d);go=false}});
