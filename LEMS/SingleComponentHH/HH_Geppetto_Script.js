b570ba135ca9dff78a84919fbfb9c8c80f6ed65b
adrianq <adrianq@users.noreply.github.com>
2015-04-11 19:52:52 +0100

    Update HH_Geppetto_Script.js

--- LEMS/SingleComponentHH/HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/HH_Geppetto_Script.js
@@ -1,4 +1,4 @@
-Simulation.addWatchLists([{name:"hhvars",variablePaths:["hhcell.electrical.SimulationTree.a","hhcell.electrical.SimulationTree.b","hhcell.electrical.SimulationTree.c", "hhcell.electrical.SimulationTree.d"]}]);
+Simulation.addWatchLists([{name:"hhvars",variablePaths:["hhcell.electrical.SimulationTree.hhpop[0].v","hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q","hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q", "hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q"]}]);
 Simulation.startWatch();
 Simulation.start();
 
@@ -8,4 +8,4 @@ Popup1.setMessage("Waiting for Neuron Simulator results");
 Popup1.setPosition(807,436)
 
 var go = true;
-GEPPETTO.on(Events.Simulation_stopped, function(){if(go){Popup1.destroy();G.addWidget(GEPPETTO.Widgets.PLOT);Plot1.setPosition(486,186);Plot1.setName("Neuron Simulator Data");Plot1.plotData(hhcell.electrical.SimulationTree.a);Plot1.plotData(hhcell.electrical.SimulationTree.b);Plot1.plotData(hhcell.electrical.SimulationTree.c);Plot1.plotData(hhcell.electrical.SimulationTree.d);go=false}});
+GEPPETTO.on(Events.Simulation_stopped, function(){if(go){Popup1.destroy();G.addWidget(GEPPETTO.Widgets.PLOT);Plot1.setPosition(486,186);Plot1.setName("Neuron Simulator Data");Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].v);Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q);Plot1.plotData(hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q);Plot1.plotData(hhpop[0].bioPhys1.membraneProperties.kChans.k.n.q);go=false}});
