2df9ac5c94cdbde7b4a2e1ea9822367f7aef9e9b
jrmartin <yeshuarmartinez@gmail.com>
2015-04-23 13:09:14 -0700

    update to recording test

--- Recording/script.js
+++ Recording/script.js
@@ -1,6 +1,6 @@
-purkinje.recordings.getSimulationTree();
+purkinje.electrical.getSimulationTree();
 
-Simulation.setWatchedVariables([purkinje.recordings.SimulationTree.P.neuron0.gi, purkinje.recordings.SimulationTree.P.neuron0.ge]);
+Simulation.setWatchedVariables([purkinje.electrical.SimulationTree.P.neuron0.gi, purkinje.electrical.SimulationTree.P.neuron0.ge]);
 Simulation.start();
 
 G.addWidget(Widgets.POPUP);
@@ -9,5 +9,5 @@ Popup1.setMessage("Testing recording values");
 Popup1.setSize(172.80000019073486,394.80000019073486)
 Popup1.setPosition(129,77)
 G.addWidget(Widgets.VARIABLEVISUALISER);
-VariableVisualizer1.setVariable(Entity.Aspect.SimulationTree.P.neuron0.gi);
-VariableVisualizer1.setPosition(529,77);
+VarVis1.setVariable(pukinje.electrical.SimulationTree.P.neuron0.gi);
+VarVis1.setPosition(529,77);
