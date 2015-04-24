a49072b682ff73b5637c423f8c276fb2fb147129
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-04-24 10:59:25 +0100

    Update HH_Geppetto_Script.js

--- LEMS/SingleComponentHH/HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/HH_Geppetto_Script.js
@@ -49,6 +49,6 @@ Popup1.setName("Description");
 Popup1.setPosition(710,398);
 Popup1.setSize(160,465)
 
-Simulation.addBrightnessFunction(hhcell.electrical, hhcell.electrical.SimulationTree.hhpop[0].v, function(x){return (x+0.06)/0.06;});
+Simulation.addBrightnessFunction(hhcell.electrical, hhcell.electrical.SimulationTree.hhpop[0].v, function(x){return (x+0.07)/0.1;});
 
 G.incrementCameraZoom(-0.15);
