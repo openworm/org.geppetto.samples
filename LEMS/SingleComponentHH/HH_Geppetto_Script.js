5d9cc91b17199542f115341bce9713597df8b7d0
jrmartin <yeshuarmartinez@gmail.com>
2015-04-23 18:07:13 -0700

    change script

--- LEMS/SingleComponentHH/HH_Geppetto_Script.js
+++ LEMS/SingleComponentHH/HH_Geppetto_Script.js
@@ -8,4 +8,4 @@ Popup1.setMessage("Waiting for Neuron Simulator results");
 Popup1.setPosition(807,436)
 
 var go = true;
-GEPPETTO.on(Events.Simulation_stopped, function(){if(go){Popup1.destroy();G.addWidget(GEPPETTO.Widgets.PLOT);Plot1.setPosition(486,186);Plot1.setName("Neuron Simulator Data");Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].v);Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q);Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.alpha););go=false}});
+GEPPETTO.on(Events.Simulation_stopped, function(){if(go){Popup1.destroy();G.addWidget(GEPPETTO.Widgets.PLOT);Plot1.setPosition(486,186);Plot1.setName("Neuron Simulator Data");Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].v);Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.q);Plot1.plotData(hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.m.alpha);go=false}});
