804b35530d61a3552d9175cc7ead427a61098c79
jrmartin <yeshuarmartinez@gmail.com>
2015-05-05 09:34:02 -0700

    add script to recording

--- Recording/script.js
+++ Recording/script.js
@@ -1,11 +1,1 @@
-Simulation.setWatchedVariables([purkinje.electrical.SimulationTree.P.neuron0.gi, purkinje.electrical.SimulationTree.P.neuron0.ge]);
-Simulation.start();
-
-G.addWidget(Widgets.POPUP);
-Popup1.setName("Recording Simulation");
-Popup1.setMessage("Testing recording values");
-Popup1.setSize(172.80000019073486,394.80000019073486)
-Popup1.setPosition(129,77)
-G.addWidget(Widgets.VARIABLEVISUALISER);
-VarVis1.setVariable(purkinje.electrical.SimulationTree.P.neuron0.gi);
-VarVis1.setPosition(529,77);
+muscle_0.mechanical.getSimulationTree();
