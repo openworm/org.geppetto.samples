84b833aca58fc2d43dbd77caa408f85f5e5f25b4
Padraig Gleeson <p.gleeson@gmail.com>
2015-06-13 14:32:54 +0200

    Adding cortex example

---
+++ obj/TVB/script_cortex.js
@@ -0,0 +1,5 @@
+G.addWidget(Widgets.POPUP);
+Popup1.setName("TheVirtualBrain Sample");
+Popup1.setMessage("This is your brain on Geppetto...\n\nAn example OBJ file taken from TheVirtualBrain project. Original data source: https://github.com/the-virtual-brain/tvb-data/tree/master/tvb_data/obj");
+Popup1.setSize(172,394)
+Popup1.setPosition(129,77)
