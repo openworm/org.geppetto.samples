7748f5a36e130fd1243888429b1a0420c49a38cd
Padraig Gleeson <p.gleeson@gmail.com>
2015-06-13 10:15:12 +0200

    Adding face example from TVB...

---
+++ obj/TVB/README.md
@@ -0,0 +1,3 @@
+These are example OBJ files taken from TheVirtualBrain project. 
+
+Original data source: https://github.com/the-virtual-brain/tvb-data/tree/master/tvb_data/obj
---
+++ obj/TVB/script_face.js
@@ -0,0 +1,17 @@
+G.addWidget(Widgets.POPUP);
+Popup1.setName("TheVirtualBrain Sample");
+Popup1.setMessage("This is an example OBJ file taken from TheVirtualBrain project. It's a face... Original data source: https://github.com/the-virtual-brain/tvb-data/tree/master/tvb_data/obj");
+Popup1.setSize(172,394)
+Popup1.setPosition(129,77)
+
+G.incrementCameraRotate(0,0.02,0)
+G.incrementCameraRotate(0,0.02,0)
+G.incrementCameraRotate(0,0.02,0)
+G.incrementCameraRotate(0,0.02,0)
+
+G.incrementCameraRotate(-0.,0.0,-0.05)
+
+G.incrementCameraRotate(0,0.02,0)
+G.incrementCameraRotate(-0.,0.0,-0.05)
+
+G.incrementCameraRotate(0,0.02,0)
