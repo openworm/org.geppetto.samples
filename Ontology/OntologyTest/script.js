6447e3ef56bf5612c357bc3f5cb42b88bdd95e1c
johnidol <giovanni.idili@gmail.com>
2016-10-19 13:59:18 +0100

    add ontology sample

---
+++ Ontology/OntologyTest/script.js
@@ -0,0 +1,7 @@
+Model.getDatasources()[0].fetchVariable('VFB_00000001');
+Model.getDatasources()[0].fetchVariable('FBbt_00100219');
+
+Instances.getInstance("VFB_00000001.VFB_00000001_meta");
+Instances.getInstance("FBbt_00100219.FBbt_00100219_meta");
+
+window.Model.getLibraries()[1].getTypes()[0].resolve();
